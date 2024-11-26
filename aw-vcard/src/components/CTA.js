258870d73330a94fa74bf321448f5f5705da2c92
eakffel <60756427+eakffel@users.noreply.github.com>
2024-11-25 20:33:01 -0500

    s

--- aw-vcard/src/components/CTA.js
+++ aw-vcard/src/components/CTA.js
@@ -12,14 +12,14 @@ function CTA(){
                     </svg>
                 </span>
             </a>
-            <a href="https://www.linkedin.com/in/amanda-whitehorn-b333b544/" className="cta-btn cta-btn-web">
+            <a href="https://westtn.consulting/" className="cta-btn cta-btn-web">
             <span className="cta-btn-logo">
                     <img src={Logo} width="65" height="65" viewBox="0 0 25 25" fill="none"/>
                 </span>
             </a>
             <a href="tel:(901)471-0451 " className="cta-btn cta-btn-email">
             <span className="cta-btn-icon">
-        <svg width="20" height="20" viewBox="0 0 24 24" stroke="#fff" stroke-width="2" stroke-linejoin="round" fill="#b9212a" xmlns="http://www.w3.org/2000/svg">
+        <svg width="2em" height="2em" viewBox="0 0 512 512" stroke="#fff" stroke-width="4" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg">
             <path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z"fill="#b9212a"/>  
 
         </svg>
